6215e3831149b118f6608ec2b997d1b3f97a8475
Gamecrafter <pro_victor@ymail.com>
2015-02-02 21:50:30 -0800

    Created Main.php

---
+++ GreetMe/src/GreetMe/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GreetMe;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."GreetMe enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."GreetMe disabled.");
+    }
+    
+    public function onPlayerJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer(); 
+        $address = $player->getAddress();      
+        $name = $player->getDisplayName();   
+        $port = $player->getPort();
+        $player->sendMessage("Welcome, ".$name.". Your IP address is: ".$address.":".$port.".");    
+    }
+}
