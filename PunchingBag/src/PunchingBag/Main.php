1677185dca9d06286df0611c8961641305bff963
Gamecrafter <pro_victor@ymail.com>
2015-02-13 12:32:03 -0800

    Created Main.php

---
+++ PunchingBag/src/PunchingBag/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PunchingBag;
+
+use pocketmine\entitiy\Villager;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."PunchingBag enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."PunchingBag disabled.");
+    }
+    
+    public function onEntityDeathEvent(EntityDeathEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Villager){
+            $event->setCancelled();
+        }
+    }
+}
