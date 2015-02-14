7776762ad43ee4f5992ec5bf9ba4833bc43bfbf2
Gamecrafter <pro_victor@ymail.com>
2015-02-13 22:54:29 -0800

    Created Main.php

---
+++ ArmorAlert/src/ArmorAlert/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ArmorAlert;
+
+use pocketmine\event\entity\EntityArmorChangeEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."ArmorAlert enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."ArmorAlert disabled.");
+    }
+    
+    public function onEntityArmorChange(EntityArmorChangeEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Player){    
+            $oldarmor = $entity->getOldItem();
+            $newarmor = $entity->getNewItem();
+            $player = $event->getPlayer();
+            $name = $player->getDisplayName();
+            $this->getServer()->broadcastMessage($name." changed from ".$oldarmor." to ".$newarmor.".");
+        }
+    }
+}
