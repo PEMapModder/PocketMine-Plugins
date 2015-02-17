74828961662ae21a0202becd575b4e15c0101099
Gamecrafter <pro_victor@ymail.com>
2015-02-16 18:42:40 -0800

    Added features

--- IPTag/src/IPTag/Main.php
+++ IPTag/src/IPTag/Main.php
@@ -2,8 +2,11 @@
 
 namespace IPTag;
 
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 class Main extends PluginBase{
     
@@ -13,5 +16,13 @@ class Main extends PluginBase{
     
     public function onDisable(){
         $this->getLogger()->info(TextFormat::RED."IPTag disabled.");
+    }
+    
+    public function onPlayerJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $address = $player->getAddress();
+        $name = $player->getDisplayName();
+        $port = $player->getPort();
+        $player->setNameTag("$name\n$address:$port");
     }
 }
