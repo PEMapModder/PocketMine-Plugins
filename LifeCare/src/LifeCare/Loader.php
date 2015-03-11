07beafe4fd9703237294d3d650b0b2816ce20899
Gamecrafter <pro_victor@ymail.com>
2015-03-11 16:52:53 -0700

    Created Loader.php

---
+++ LifeCare/src/LifeCare/Loader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LifeCare;
+
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase implements Listener{
+
+}
