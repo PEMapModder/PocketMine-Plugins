5a5cfad10d3a0d93986d930b5e3cd5c68c787734
Gamecrafter <pro_victor@ymail.com>
2015-03-10 19:26:21 -0700

    Created Loader.php

---
+++ SignsPro/src/SignsPro/Loader.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SignsPro;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Sign;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."SignsPro enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."SignsPro disabled.");
+    }
+    
+    public function onBlockBreak(BlockBreakEvent $event){
+    
+    }
+    
+    public function onBlockPlace(BlockPlaceEvent $event){
+    
+    }
+    
+    public function onSignChange(SignChangeEvent $event){
+        if($event->getPlayer()->hasPermission("signspro.action.create")){
+            if($event->getLine(0) === ""){
+                
+            }
+            elseif($event->getLine(0) === ""){
+                
+            }
+            elseif($event->getLine(0) === ""){
+                
+            }
+        }
+        else{
+            $sender->sendMessage("You don't have permissions to create signs.");
+        }
+    }
+    
+    public function onPlayerInteract(PlayerInteractEvent $event){
+        if($event->getPlayer()->hasPermission("signspro.action.use")){
+            
+        }
+        else{
+            $sender->sendMessage("You don't have permission to use signs.");
+        }
+    }
+}
