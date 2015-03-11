3d0146b52b8e138b3e259e22131b41d3d95f2345
Gamecrafter <pro_victor@ymail.com>
2015-03-11 16:57:15 -0700

    Update Loader.php

--- LifeCare/src/LifeCare/Loader.php
+++ LifeCare/src/LifeCare/Loader.php
@@ -10,4 +10,21 @@ use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase implements Listener{
 
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."LifeCare enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."LifeCare disabled.");
+    }
+    
+    public function onEntityDamage(EntityDamageEvent $event){
+      
+    }
+    
+    public function onEntityRegainHealth(EntityRegainHealthEvent $event){
+      
+    }
 }
