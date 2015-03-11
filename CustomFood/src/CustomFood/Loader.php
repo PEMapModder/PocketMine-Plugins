8f4dfee86130d93ebb3da6fc086b9a8599ec58b9
Gamecrafter <pro_victor@ymail.com>
2015-03-10 21:41:43 -0700

    Update Loader.php

--- CustomFood/src/CustomFood/Loader.php
+++ CustomFood/src/CustomFood/Loader.php
@@ -11,7 +11,7 @@ use pocketmine\utils\TextFormat;
 class Loader extends PluginBase implements Listener{
 
     public function onEnable(){
-        $this-saveDefaultConfig();
+        $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN."CustomFood enabled.");
     }
