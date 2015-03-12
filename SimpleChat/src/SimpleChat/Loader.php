3c08697266c51e04356d5f14bab4ba150f0f6ebc
Gamecrafter <pro_victor@ymail.com>
2015-03-11 20:47:03 -0700

    Created Loader.php

---
+++ SimpleChat/src/SimpleChat/Loader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SimpleChat;
+
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase implements Listener{
+
+}
