47d4133745eb44f28577c88693d7ca9937c8e028
Gamecrafter <pro_victor@ymail.com>
2015-03-11 20:48:47 -0700

    Update Loader.php

--- SimpleChat/src/SimpleChat/Loader.php
+++ SimpleChat/src/SimpleChat/Loader.php
@@ -9,4 +9,17 @@ use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase implements Listener{
 
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."SimpleChat enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."SimpleChat disabled.");
+    }
+    
+    public function onPlayerChat(PlayerChatEvent $event){
+        
+    }
 }
