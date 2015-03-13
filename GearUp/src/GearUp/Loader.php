f0c0c41a46f6811bbe56a4d05a24185bac4e9385
Gamecrafter <pro_victor@ymail.com>
2015-03-13 12:35:31 -0700

    Update Loader.php

--- GearUp/src/GearUp/Loader.php
+++ GearUp/src/GearUp/Loader.php
@@ -22,20 +22,52 @@ class Loader extends PluginBase{
         if(strtolower($command->getName()) === "gear"){
             if(isset($args[0])){
                 if($sender instanceof Player){
+                    if($args[0] === "0"){
+                        $sender->getInventory()->setBoots(0);
+                        $sender->getInventory()->setChestplate(0);
+                        $sender->getInventory()->setHelmet(0);
+                        $sender->getInventory()->setLeggings(0);
+                        $sender->sendMessage("");
+                    }
                     if($args[0] === "1"){
-                    
+                        $sender->getInventory()->setBoots(301);
+                        $sender->getInventory()->setChestplate(299);
+                        $sender->getInventory()->setHelmet(298);
+                        $sender->getInventory()->setLeggings(300);
+                        $sender->getInventory()->setItemInHand();
+                        $sender->sendMessage("");
                     }
                     if($args[0] === "2"){
-                    
+                        $sender->getInventory()->setBoots(305);
+                        $sender->getInventory()->setChestplate(303);
+                        $sender->getInventory()->setHelmet(302);
+                        $sender->getInventory()->setLeggings(304);
+                        $sender->getInventory()->setItemInHand();
+                        $sender->sendMessage("");
                     }
                     if($args[0] === "3"){
-                    
+                        $sender->getInventory()->setBoots(309);
+                        $sender->getInventory()->setChestplate(307);
+                        $sender->getInventory()->setHelmet(306);
+                        $sender->getInventory()->setLeggings(308);
+                        $sender->getInventory()->setItemInHand();
+                        $sender->sendMessage("");
                     }
                     if($args[0] === "4"){
-                    
+                        $sender->getInventory()->setBoots(317);
+                        $sender->getInventory()->setChestplate(315);
+                        $sender->getInventory()->setHelmet(314);
+                        $sender->getInventory()->setLeggings(316);
+                        $sender->getInventory()->setItemInHand();
+                        $sender->sendMessage("");
                     }
                     if($args[0] === "5"){
-                    
+                        $sender->getInventory()->setBoots(313);
+                        $sender->getInventory()->setChestplate(311);
+                        $sender->getInventory()->setHelmet(310);
+                        $sender->getInventory()->setLeggings(312);
+                        $sender->getInventory()->setItemInHand();
+                        $sender->sendMessage("");
                     }
                     else{
                         $sender->sendMessage("");
