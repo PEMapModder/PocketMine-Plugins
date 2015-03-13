8f100ceaa213a4fb92e1a8aef34fbce038bc7332
Gamecrafter <pro_victor@ymail.com>
2015-03-12 18:55:56 -0700

    Created Loader.php

---
+++ GearUp/src/GearUp/Loader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GearUp;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase{
+
+}
