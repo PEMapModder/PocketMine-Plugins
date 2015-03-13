de5f9aa45a0e2b2962540b1102579058f81b7f6f
Gamecrafter <pro_victor@ymail.com>
2015-03-13 12:19:55 -0700

    Update Loader.php

--- GearUp/src/GearUp/Loader.php
+++ GearUp/src/GearUp/Loader.php
@@ -6,6 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 class Loader extends PluginBase{
 
@@ -20,27 +21,32 @@ class Loader extends PluginBase{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "gear"){
             if(isset($args[0])){
-                if($args[0] === "1"){
+                if($sender instanceof Player){
+                    if($args[0] === "1"){
                     
-                }
-                if($args[0] === "2"){
+                    }
+                    if($args[0] === "2"){
                     
-                }
-                if($args[0] === "3"){
+                    }
+                    if($args[0] === "3"){
                     
-                }
-                if($args[0] === "4"){
+                    }
+                    if($args[0] === "4"){
                     
-                }
-                if($args[0] === "5"){
+                    }
+                    if($args[0] === "5"){
                     
+                    }
+                    else{
+                        $sender->sendMessage("");
+                    }
                 }
                 else{
-                    $sender->sendMessage("");
+                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
                 }
             }
             if(isset($args[1])){
-                
+                $target = $sender->getServer()->getPlayer($args[1]);
             }
             else{
                 
