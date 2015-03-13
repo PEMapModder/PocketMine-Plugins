6fb781873cd83cf85a5cc9de83ee43b1142f8cc2
Gamecrafter <pro_victor@ymail.com>
2015-03-12 19:05:11 -0700

    Update Loader.php

--- GearUp/src/GearUp/Loader.php
+++ GearUp/src/GearUp/Loader.php
@@ -9,4 +9,17 @@ use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
 
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."GearUp enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."GearUp disabled.");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "gear"){
+          
+        }
+    }
 }
