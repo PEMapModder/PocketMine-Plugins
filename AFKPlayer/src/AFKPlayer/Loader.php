314bc511348b9447c6412c6d72bd4b374e8716f4
Gamecrafter <pro_victor@ymail.com>
2015-03-14 12:25:49 -0700

    Update Loader.php

--- AFKPlayer/src/AFKPlayer/Loader.php
+++ AFKPlayer/src/AFKPlayer/Loader.php
@@ -4,13 +4,18 @@ namespace AFKPlayer;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 
-class Loader extends PluginBase{
+class Loader extends PluginBase implements Listener{
 
     public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN."AFKPlayer enabled.");
     }
     
@@ -23,9 +28,11 @@ class Loader extends PluginBase{
             if(strtolower($command->getName()) === "afk"){
                 if(isset($args[0])){
                     if(strtolower($args[0]) === "off"){
+                        $sender->showPlayer($sender);
                         $sender->sendMessage("You are not AFK anymore.");
                     }
                     if(strtolower($args[0]) === "on"){
+                        $sender->hidePlayer($sender);
                         $sender->sendMessage("You are now AFK. Run /afk off to turn it off.");
                     }
                     else{
