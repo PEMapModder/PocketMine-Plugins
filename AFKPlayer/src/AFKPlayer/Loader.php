1018bacc5508a4535f8cad2947756a0f521e348b
Gamecrafter <pro_victor@ymail.com>
2015-03-13 21:26:40 -0700

    Update Loader.php

--- AFKPlayer/src/AFKPlayer/Loader.php
+++ AFKPlayer/src/AFKPlayer/Loader.php
@@ -9,4 +9,11 @@ use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
 
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."AFKPlayer enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."AFKPlayer disabled.");
+    }
 }
