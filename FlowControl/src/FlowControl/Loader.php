bb325196e2391a7ac1e270521f6a0b56ae246f63
Gamecrafter <pro_victor@ymail.com>
2015-03-14 14:29:15 -0700

    Update Loader.php

--- FlowControl/src/FlowControl/Loader.php
+++ FlowControl/src/FlowControl/Loader.php
@@ -23,10 +23,10 @@ class Loader extends PluginBase implements Listener{
     
     public function onBlockSpread(BlockSpreadEvent $event){
         if($event->getBlock() instanceof Water && $this->getConfig()->get("spread-water") === false){
-            $event->setCancelled();
+            $event->setCancelled(true);
         }
         if($event->getBlock() instanceof Lava && $this->getConfig()->get("spread-lava") === false){
-            $event->serCancelled();
+            $event->serCancelled(true);
         }
     }
 }
