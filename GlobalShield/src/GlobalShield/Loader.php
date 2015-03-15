485219166ad741aa42568715629b52ce65066156
Gamecrafter <pro_victor@ymail.com>
2015-03-15 16:49:50 -0700

    Update Loader.php

--- GlobalShield/src/GlobalShield/Loader.php
+++ GlobalShield/src/GlobalShield/Loader.php
@@ -14,6 +14,7 @@ use pocketmine\utils\TextFormat;
 class Loader extends PluginBase implements Listener{
     
     public function onEnable(){
+        $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN."GlobalShield enabled.");
     }
