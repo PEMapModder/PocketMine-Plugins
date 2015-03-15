dd5ea2ca415c2464d089c319029a468d7cf594bd
Gamecrafter <pro_victor@ymail.com>
2015-03-14 21:54:09 -0700

    Update Loader.php

--- ServerStaff/src/ServerStaff/Loader.php
+++ ServerStaff/src/ServerStaff/Loader.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace ServerStaff;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase{
+    
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."ServerStaff enabled.");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."ServerStaff disabled.");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "staff"{
+
+        }
+    }
+}
