08e14b2f289411c23a4f3821d6a330cd7993e82d
Gamecrafter <pro_victor@ymail.com>
2015-03-15 17:16:12 -0700

    Update Loader.php

--- WarpArrow/src/WarpArrow/Loader.php
+++ WarpArrow/src/WarpArrow/Loader.php
@@ -2,10 +2,20 @@
 
 namespace WarpArrow;
 
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class Loader extends PluginBase{
+class Loader extends PluginBase implements Listener{
 
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN."WarpArrow enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."WarpArrow disabled.");
+    }
 }
