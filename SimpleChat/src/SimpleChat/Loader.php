853ee4c1568db2c9868b63463aeccfb63ddbb94a
Gamecrafter <pro_victor@ymail.com>
2015-03-18 12:35:53 -0700

    Update Loader.php

--- SimpleChat/src/SimpleChat/Loader.php
+++ SimpleChat/src/SimpleChat/Loader.php
@@ -20,6 +20,6 @@ class Loader extends PluginBase implements Listener{
     }
     
     public function onPlayerChat(PlayerChatEvent $event){
-        $event->setFormat(str_replace(["{ip}", "{message}", "{player}", "{port}"], [$event->getPlayer()->getAddress(), $event->getMessage(), $event->getPlayer()->getName(), $event->getPlayer()->getPort()], $this->getConfig()->get("format")));
+        $event->setFormat(str_replace(["{ip}", "{level}", "{message}", "{player}", "{port}"], [$event->getPlayer()->getAddress(), $event->getPlayer()->getLevel()->getName(), $event->getMessage(), $event->getPlayer()->getName(), $event->getPlayer()->getPort()], $this->getConfig()->get("format")));
     }
 }
