62ea3bbd054631e2b14d8a106214c076dd840140
Gamecrafter <pro_victor@ymail.com>
2015-03-19 20:23:43 -0700

    Update Loader.php

--- ServerRules/src/ServerRules/Loader.php
+++ ServerRules/src/ServerRules/Loader.php
@@ -9,4 +9,20 @@ use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
 
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."ServerRules enabled.");
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."ServerRules disabled.");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "rules"){
+            foreach($this->getConfig()->get("rules") as $rules){
+                $sender->sendMessage($rules);
+            }
+        }
+        return true;
+    }
 }
