12cb9fb3b0dc5a188efdf632d857b35bf1757dcc
Gamecrafter <pro_victor@ymail.com>
2015-03-20 16:02:40 -0700

    Update Loader.php

--- GoTroll/src/GoTroll/Loader.php
+++ GoTroll/src/GoTroll/Loader.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace GoTroll;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase{
+    
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."GoTroll enabled.");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."GoTroll disabled.");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "troll"){
+          
+        }
+    }
+}
