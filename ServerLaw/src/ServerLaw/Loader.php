cb976335cc205a39958fa838d9c86ce3b810e490
Gamecrafter <pro_victor@ymail.com>
2015-03-19 20:32:21 -0700

    Update Loader.php

--- ServerLaw/src/ServerLaw/Loader.php
+++ ServerLaw/src/ServerLaw/Loader.php
@@ -2,3 +2,24 @@
 
 namespace ServerLaw;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase{
+    
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."ServerLaw enabled.");
+    }
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."ServerLaw disabled.");
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "laws"){
+            foreach($this->getConfig()->get("laws") as $laws){
+                $sender->sendMessage($laws);
+            }
+        }
+        return true;
+    }
