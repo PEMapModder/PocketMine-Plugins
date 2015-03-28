0f93ea7af8e02b38d2ef72ed327faa96fcc82a62
Gamecrafter <pro_victor@ymail.com>
2015-03-28 13:03:23 -0700

    Update Loader.php

--- SwitchIt/src/SwitchIt/Loader.php
+++ SwitchIt/src/SwitchIt/Loader.php
@@ -8,6 +8,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
+    
     public function onEnable(){
         $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
     }
