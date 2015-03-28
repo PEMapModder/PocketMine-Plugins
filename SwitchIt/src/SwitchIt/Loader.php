533cff9797b49c15dd6271d31ab511377dd44460
Gamecrafter <pro_victor@ymail.com>
2015-03-28 13:03:10 -0700

    Create Loader.php

---
+++ SwitchIt/src/SwitchIt/Loader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SwitchIt;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase{
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."Disabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "disable"){
+        
+        }
+        if(strtolower($command->getName()) === "enable"){
+        
+        }
+    }
+}
