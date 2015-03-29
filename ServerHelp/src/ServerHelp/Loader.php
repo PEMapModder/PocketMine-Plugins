a1091201b35295b0ee6da6a28819755552b1df1f
Gamecrafter <pro_victor@ymail.com>
2015-03-29 16:45:18 -0700

    Create Loader.php

---
+++ ServerHelp/src/ServerHelp/Loader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ServerHelp;
+
+use pocketmine\event\player\PlayerCommandPreProcessEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Loader extends PluginBase implements Listener{
+    
+    public function onEnable(){
+    	$this->saveDefaultConfig();
+    	if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
+    	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    	}
+    	else{
+    	    $this->getLogger()->info(TextFormat::YELLOW."Your configuration file is outdated.");
+    	    $this->getPluginLoader()->disablePlugin($this);
+    	}
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."Disabling ".$this->getDescription()->getFullName()."...");
+    }
+
+    public function onPlayerCommandPreProcess(PlayerPreProcessEvent $event){
+
+    }
+}
