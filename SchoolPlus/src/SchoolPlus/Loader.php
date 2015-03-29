614f54ed1cffda51a90d68e087b89532e5080c71
Gamecrafter <pro_victor@ymail.com>
2015-03-28 17:34:52 -0700

    Update Loader.php

--- SchoolPlus/src/SchoolPlus/Loader.php
+++ SchoolPlus/src/SchoolPlus/Loader.php
@@ -6,5 +6,12 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
-
+  
+    public function onEnable(){
+        $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED."Disabling ".$this->getDescription()->getFullName()."...");
+    }
 }
