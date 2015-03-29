d1402460cd9f496519d577999400d836eb9c7965
Gamecrafter <pro_victor@ymail.com>
2015-03-28 17:40:20 -0700

    Update Loader.php

--- LifeCare/src/LifeCare/Loader.php
+++ LifeCare/src/LifeCare/Loader.php
@@ -13,9 +13,15 @@ use pocketmine\utils\TextFormat;
 class Loader extends PluginBase implements Listener{
 
     public function onEnable(){
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    	$this->saveDefaultConfig();
+    	if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
+    	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->getLogger()->info(TextFormat::GREEN."Enabling ".$this->getDescription()->getFullName()."...");
+    	}
+    	else{
+    	    $this->getLogger()->info(TextFormat::YELLOW."Your configuration file is outdated.");
+    	    $this->getPluginLoader()->disablePlugin($this);
+    	}
     }
     
     public function onDisable(){
