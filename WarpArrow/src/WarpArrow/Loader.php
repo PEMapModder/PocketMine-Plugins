083983b29b5e8134759d76cf375aae1fa11c9f02
Gamecrafter <pro_victor@ymail.com>
2015-03-28 17:44:12 -0700

    Update Loader.php

--- WarpArrow/src/WarpArrow/Loader.php
+++ WarpArrow/src/WarpArrow/Loader.php
@@ -2,6 +2,7 @@
 
 namespace WarpArrow;
 
+use pocketmine\entity\Arrow;
 use pocketmine\event\entity\ProjectileHitEvent;
 use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
@@ -17,5 +18,11 @@ class Loader extends PluginBase implements Listener{
     
     public function onDisable(){
         $this->getLogger()->info(TextFormat::RED."Disabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onProjectileHit(ProjectileHitEvent $event){
+        if($event->getEntity() instanceof Arrow){
+            
+        }
     }
 }
