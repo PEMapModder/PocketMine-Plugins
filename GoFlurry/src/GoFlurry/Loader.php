8e4b4a96c2c3dc23e1e2cc1b4d61d943f4c9540e
Gamecrafter <pro_victor@ymail.com>
2015-03-28 18:25:38 -0700

    Update Loader.php

--- GoFlurry/src/GoFlurry/Loader.php
+++ GoFlurry/src/GoFlurry/Loader.php
@@ -2,9 +2,20 @@
 
 namespace GoFlurry;
 
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\RemoveEntityPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase{
 
+    public function onEnable(){
+      
+    }
+
+    public function onDisable(){
+      
+    }
 }
