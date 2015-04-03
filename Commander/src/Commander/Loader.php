47a7b6f862ba50531665a69c43b5217306c2fe6f
Gamecrafter <pro_victor@ymail.com>
2015-04-03 13:37:12 -0700

    Update Loader.php

--- Commander/src/Commander/Loader.php
+++ Commander/src/Commander/Loader.php
@@ -44,15 +44,14 @@ class Loader extends PluginBase implements Listener{
     public function onBlockBreak(BlockBreakEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-block-break"] === true){
             foreach($this->getConfig()->get("run")["on-block-break"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command)
- ;           }
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));           }
         }
     }
     
     public function onBlockPlace(BlockPlaceEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-block-break"] === true){
             foreach($this->getConfig()->get("run")["on-block-place"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);  
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }       
         }
     }
@@ -60,7 +59,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerBedEnter(PlayerBedEnterEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-bed-enter"] === true){
             foreach($this->getConfig()->get("run")["on-player-bed-enter"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }   
     }
@@ -68,7 +67,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerBedLeaveEvent(PlayerBedLeaveEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-bed-leave"] === true){
             foreach($this->getConfig()->get("run")["on-player-bed-leave"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }  
     }
@@ -76,7 +75,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerBucketEmpty(PlayerBucketEmptyEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-bucket-empty"] === true){
             foreach($this->getConfig()->get("run")["on-player-bucket-empty"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }
     }
@@ -84,7 +83,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerBucketFill(PlayerBucketFillEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-bucket-fill"] === true){
             foreach($this->getConfig()->get("run")["on-player-bucket-fill"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command); 
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         } 
     }
@@ -92,7 +91,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerDeath(PlayerDeathEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-death"] === true){
             foreach($this->getConfig()->get("run")["on-player-death"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             } 
         }
     }
@@ -100,7 +99,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerDropItem(PlayerDropItemEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-drop-item"] === true){
             foreach($this->getConfig()->get("run")["on-player-drop-item"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }
     }
@@ -108,7 +107,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerGameModeChange(PlayerGameModeChangeEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-gamemode-change"] === true){
             foreach($this->getConfig()->get("run")["on-player-gamemode-change"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }
     }
@@ -116,7 +115,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerInteract(PlayerInteractEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-interact"] === true){
             foreach($this->getConfig()->get("run")["on-player-interact"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }
     }
@@ -124,7 +123,7 @@ class Loader extends PluginBase implements Listener{
     public function onPlayerItemConsume(PlayerItemConsumeEvent $event){
         if($this->getConfig()->get("enable")["command"]["on-player-item-consume"] === true){
             foreach($this->getConfig()->get("run")["on-player-item-consume"] as $command){
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender, $command);
+                $this->getServer()->dispatchCommand(new ConsoleCommandSender, str_replace("{player}", $event->getPlayer()->getName(), $command));  
             }
         }
     }
