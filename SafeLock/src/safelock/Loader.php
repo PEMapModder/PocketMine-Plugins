b3c6ec7daf3dc8d6f4474709ea71d3b2c01624c2
Gamecrafter <pro_victor@ymail.com>
2015-04-06 12:24:18 -0700

    Update Loader.php

--- SafeLock/src/safelock/Loader.php
+++ SafeLock/src/safelock/Loader.php
@@ -10,15 +10,15 @@ use pocketmine\plugin\PluginBase;
 class Loader extends PluginBase implements Listener{
 
     public function onEnable(){
-    	  $this->saveDefaultConfig();
-    	  if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
-    	      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    	$this->saveDefaultConfig();
+    	if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
+    	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
             $this->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
         }
         else{
-    	      $this->getLogger()->info("§eYour configuration file is outdated.");
-    	      $this->getPluginLoader()->disablePlugin($this);
-    	  }
+    	    $this->getLogger()->info("§eYour configuration file is outdated.");
+    	    $this->getPluginLoader()->disablePlugin($this);
+    	}
     }
     
     public function onDisable(){
