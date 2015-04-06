7cc13707bfbcf07417b5a295c0d8d15e5ba0851a
Gamecrafter <pro_victor@ymail.com>
2015-04-06 15:05:32 -0700

    Update Loader.php

--- MyTag/src/mytag/Loader.php
+++ MyTag/src/mytag/Loader.php
@@ -97,12 +97,15 @@ class Loader extends PluginBase implements Listener{
     }
     
     public function onPlayerJoin(PlayerJoinEvent $event){
-        if($this->getConfig()->get("auto-set") === true && $event->getPlayer()->isOp()){
-	    $event->getPlayer()->setNameTag("[".$this->getConfig()->get("op-tag")."] ".$sender->getName());
+	if(file_exists($this->getDataFolder()."data/".$event->getPlayer()->getName().".yml")){
+		
+	}
+	else{
+		
 	}
     }
 	
     public function onPlayerQuit(PlayerQuitEvent $event){
-
+	
     }
 }
