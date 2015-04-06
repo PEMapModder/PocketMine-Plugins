183f69bcedfc195bbe0a92fea4e3b90b5bbb60b2
Gamecrafter <pro_victor@ymail.com>
2015-04-06 13:16:32 -0700

    Update Loader.php

--- DoMath/src/domath/Loader.php
+++ DoMath/src/domath/Loader.php
@@ -20,62 +20,58 @@ class Loader extends PluginBase{
         if(strtolower($command->getName()) === "add"){
             if(isset($args[0]) && isset($args[1])){
                 if(is_numeric($args[0]) && is_numeric($args[1])){
-                    $answer = $args[0] + $args[1];
-                    $sender->sendMessage(TextFormat::RED.$args[0].TextFormat::WHITE." + ".TextFormat::BLUE.$args[1].TextFormat::WHITE." = ".TextFormat::GREEN.$answer);
+                    $sender->sendMessage($args[0]." + ".$args[1]." = ".($args[0] + $args[1]));
                 }
                 else{
-                    $sender->sendMessage(TextFormat::RED."Please use numbers.");
+                    $sender->sendMessage("Please use numbers.");
                 }
             }
             else{
-                $sender->sendMessage(TextFormat::RED."Usage: ".$command->getUsage());
+                $sender->sendMessage("Usage: ".$command->getUsage());
             }
         }
         if(strtolower($command->getName()) === "divide"){
             if(isset($args[0]) && isset($args[1])){
                 if(is_numeric($args[0]) && is_numeric($args[1])){
                     if($args[1] === 0){
-                        $sender->sendMessage(TextFormat::RED."Undefined value error: 0");
+                        $sender->sendMessage("Undefined value error: 0");
                     }
                     else{
-                        $answer = $args[0] / $args[1];
-                        $sender->sendMessage(TextFormat::RED.$args[0].TextFormat::WHITE." / ".TextFormat::BLUE.$args[1].TextFormat::WHITE." = ".TextFormat::GREEN.$answer);
+                        $sender->sendMessage($args[0]." / ".$args[1]." = ".($args[0] / $args[1]));
                     }
                 } 
                 else{
-                    $sender->sendMessage(TextFormat::RED."Please use numbers.");
+                    $sender->sendMessage("Please use numbers.");
                 }
             }
             else{
-                $sender->sendMessage(TextFormat::RED."Usage: ".$command->getUsage()); 
+                $sender->sendMessage("Usage: ".$command->getUsage()); 
             }
         }
         if(strtolower($command->getName()) === "multiply"){
             if(isset($args[0]) && isset($args[1])){
                 if(is_numeric($args[0]) && is_numeric($args[1])){
-                    $answer = $args[0] * $args[1];
-                    $sender->sendMessage(TextFormat::RED.$args[0].TextFormat::WHITE." * ".TextFormat::BLUE.$args[1].TextFormat::WHITE." = ".TextFormat::GREEN.$answer);
+                    $sender->sendMessage($args[0]." * ".$args[1]." = ".($args[0] * $args[1]));
                 }
                 else{
-                    $sender->sendMessage(TextFormat::RED."Please use numbers.");
+                    $sender->sendMessage("Please use numbers.");
                 }
             }
             else{
-                $sender->sendMessage(TextFormat::RED."Usage: ".$command->getUsage());
+                $sender->sendMessage("Usage: ".$command->getUsage());
             }
         }
         if(strtolower($command->getName()) === "subtract"){
             if(isset($args[0]) && isset($args[1])){
                 if(is_numeric($args[0]) && is_numeric($args[1])){
-                    $answer = $args[0] - $args[1];
-                    $sender->sendMessage(TextFormat::RED.$args[0].TextFormat::WHITE." - ".TextFormat::BLUE.$args[1].TextFormat::WHITE." = ".TextFormat::GREEN.$answer);
+                    $sender->sendMessage($args[0]." - ".$args[1]." = ".($args[0] - $args[1]));
                 }
                 else{
-                    $sender->sendMessage(TextFormat::RED."Please use numbers.");
+                    $sender->sendMessage("Please use numbers.");
                 }
             }
             else{
-                $sender->sendMessage(TextFormat::RED."Usage: ".$command->getUsage());
+                $sender->sendMessage("Usage: ".$command->getUsage());
             }
         }
         return true;
