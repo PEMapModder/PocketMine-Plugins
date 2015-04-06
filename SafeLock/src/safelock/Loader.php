9d21df23368f62bb88a8ea7dc6fc82339c159163
Gamecrafter <pro_victor@ymail.com>
2015-04-06 12:23:37 -0700

    Create Loader.php

---
+++ SafeLock/src/safelock/Loader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace safelock;
+
+use pocketmine\block\WoodDoor;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase implements Listener{
+
+    public function onEnable(){
+    	  $this->saveDefaultConfig();
+    	  if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
+    	      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
+        }
+        else{
+    	      $this->getLogger()->info("§eYour configuration file is outdated.");
+    	      $this->getPluginLoader()->disablePlugin($this);
+    	  }
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event){
+    
+    }
+}
