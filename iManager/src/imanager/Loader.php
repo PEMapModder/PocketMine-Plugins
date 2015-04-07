b5bb88cdddc17cd69f45936d3bed3bf5d13acd62
Gamecrafter <pro_victor@ymail.com>
2015-04-06 19:34:22 -0700

    Update Loader.php

--- iManager/src/imanager/Loader.php
+++ iManager/src/imanager/Loader.php
@@ -44,13 +44,22 @@ class Loader extends PluginBase implements Listener{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     	if(strtolower($command->getName()) === "imanager"){
     	    if(isset($args[0])){
+    	    	if(strtolower($args[0]) === "addip"){
+    	    		
+    	    	} 
     	    	if(strtolower($args[0]) === "addresslist"){
 
+    	    	}
+    	    	if(strtolower($args[0]) === "delip"){
+    	    		
     	    	}
     	    	if(strtolower($args[0]) === "gamemodelist"){
     	    		
     	    	}
     	    	if(strtolower($args[0]) === "healthlist"){
+    	    		
+    	    	}
+    	    	if(strtolower($args[0]) === "info"){
     	    		
     	    	}
     	    	if(strtolower($args[0]) === "kickall"){
@@ -79,83 +88,12 @@ class Loader extends PluginBase implements Listener{
     	    	$sender->sendMessage();
     	    }
     	}
-    	if(strtolower($command->getName()) === "addresslist"){
-    	    $sender->sendMessage(TextFormat::YELLOW."IP address and port of all players that are currently online:");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){  
-    	    	$sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::RED.$players->getAddress().TextFormat::BLUE.":".TextFormat::GREEN.$players->getPort());
-    	    }	
-    	}
-    	if(strtolower($command->getName()) === "gamemodelist"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Gamemode of all players that are currently online:");
-            foreach($sender->getServer()->getOnlinePlayers() as $players){
-                if($players->isAdventure()){
-                    $sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::LIGHT_PURPLE."2");
-                }
-                elseif($players->isCreative()){
-                    $sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::GREEN."1");
-                }
-                elseif($players->isSurvival()){
-                    $sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::BLUE."0");
-                }
-                else{
-                    $sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::RED."?");
-                }
-            }
-    	}
-    	if(strtolower($command->getName()) === "healthlist"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Health of all players that are currently online:");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){
-    	    	$sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.": ".TextFormat::RED.$players->getHealth().TextFormat::BLUE."/".TextFormat::GREEN.$players->getMaxHealth());
-    	    }
-    	}
-    	if(strtolower($command->getName()) === "kickall"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Kicked everyone that is not an OP in the server.");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){
-    	    	if($players->hasPermission("imanager.exempt")){
-                    $players->sendMessage("You are safe!");
-                }
-                else{
-                    $players->kick();
-                }
-    	    }
-    	}
-    	if(strtolower($command->getName()) === "killall"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Killed everyone that is not an OP in the server.");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){               
-    	    	if($players->hasPermission("imanager.exempt")){
-                    $players->sendMessage("You are safe!");
-                }
-                else{
-                    $players->kill();
-                }
-    	    }
-    	}
-    	if(strtolower($command->getName()) === "opall"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Opped everyone in the server.");
-            foreach($sender->getServer()->getOnlinePlayers() as $players){
-                $players->setOp(true);
-            }	
-    	}
-    	if(strtolower($command->getName()) === "oplist"){
-    	    $sender->sendMessage(TextFormat::YELLOW."OPs that are currently online:");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){
-    	    	if($players->isOp()){
-    	    	    $sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName());
-    	    	}
-    	    }	
-    	}
-    	if(strtolower($command->getName()) === "poslist"){
-    	    $sender->sendMessage(TextFormat::YELLOW."Location of all players that are currently online:");
-    	    foreach($sender->getServer()->getOnlinePlayers() as $players){
-    	    	$sender->sendMessage(TextFormat::YELLOW."> ".TextFormat::WHITE.$players->getName().TextFormat::YELLOW.":".TextFormat::WHITE." X: ".TextFormat::RED.$players->getX().TextFormat::WHITE." Y: ".TextFormat::BLUE.$players->getY().TextFormat::WHITE." Z: ".TextFormat::GREEN.$players->getZ().TextFormat::WHITE." Level: ".TextFormat::LIGHT_PURPLE.$players->getLevel()->getName());
-    	    }
-    	}
-        return true;
+    	return true;
     }
     
     public function onPlayerChat(PlayerChatEvent $event){
     	if($this->getConfig()->get("enable")["chat-log"] === true){
-    		
+    	    $this->chat->set($event->getPlayer()->getName() => $event->getMessage());
     	}
     }
     
