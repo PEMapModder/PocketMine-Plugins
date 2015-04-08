f4a244434e1ee548eb4c91b5214fc83e37987430
Gamecrafter <pro_victor@ymail.com>
2015-04-08 09:16:42 -0700

    Update Loader.php

--- LocatorPro/src/locatorpro/Loader.php
+++ LocatorPro/src/locatorpro/Loader.php
@@ -33,50 +33,49 @@ class Loader extends PluginBase implements Listener{
     }
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        if(strtolower($command->getName()) === "getpos"){
-            if(isset($args[0])){
-                if($sender->hasPermission("locator.command.getpos.other")){
-                    $target = $sender->getServer()->getPlayer($args[0]);
-                    if($target !== null){
-                        $sender->sendMessage($target->getName()."'s location:");
-                        $sender->sendMessage("X: ".$target->getFloorX());
-                        $sender->sendMessage("Y: ".$target->getFloorY());
-                        $sender->sendMessage("Z: ".$target->getFloorZ());
-                        $sender->sendMessage("Level: ".$target->getLevel()->getName());
-                        $sender->sendMessage("Yaw: ".$target->getYaw());
-                        $sender->sendMessage("Pitch: ".$target->getPitch());
-                    }
-                    else{
-                        $sender->sendMessage("§cPlease specify a valid player.");
-                    }
-                }
-                else{
-                    $sender->sendMessage("§cYou don't have permissions to use this command.");
-                }
+        if(strtolower($command->getName()) === "locatorpro"){
+            if(strtolower($args[0]) === "back"){
+                
             }
-            else{
-                if($sender instanceof Player){
-                    if($sender->hasPermission("locator.command.getpos.self")){
-                        $sender->sendMessage("Your location:");
-                        $sender->sendMessage("X: ".$sender->getFloorX());
-                        $sender->sendMessage("Y: ".$sender->getFloorY());
-                        $sender->sendMessage("Z: ".$sender->getFloorZ());
-                        $sender->sendMessage("Level: ".$sender->getLevel()->getName());
-                        $sender->sendMessage("Yaw: ".$sender->getYaw());
-                        $sender->sendMessage("Pitch: ".$sender->getPitch());
+            if(strtolower($args[0]) === "pos"){
+                if(isset($args[1])){
+                    if($sender->hasPermission("locator.command.locator.other")){
+                        $target = $sender->getServer()->getPlayer($args[1]);
+                        if($target !== null){
+                            $sender->sendMessage($target->getName()."'s location:");
+                            $sender->sendMessage("X: ".$target->getFloorX());
+                            $sender->sendMessage("Y: ".$target->getFloorY());
+                            $sender->sendMessage("Z: ".$target->getFloorZ());
+                            $sender->sendMessage("Level: ".$target->getLevel()->getName());
+                            $sender->sendMessage("Yaw: ".$target->getYaw());
+                            $sender->sendMessage("Pitch: ".$target->getPitch());
+                        }
+                        else{
+                            $sender->sendMessage("§cPlease specify a valid player.");
+                        }
                     }
                     else{
                         $sender->sendMessage("§cYou don't have permissions to use this command.");
                     }
                 }
                 else{
-                    $sender->sendMessage("§cPlease run this command in-game.");
-                }
-            }
-        }
-        if(strtolower($command->getName()) === "locatorpro"){
-            if(strtolower($args[0]) === "back"){
-                
+                    if($sender instanceof Player){
+                        if($sender->hasPermission("locator.command.locator.self")){
+                            $sender->sendMessage("Your location:");
+                            $sender->sendMessage("X: ".$sender->getFloorX());
+                            $sender->sendMessage("Y: ".$sender->getFloorY());
+                            $sender->sendMessage("Z: ".$sender->getFloorZ());
+                            $sender->sendMessage("Level: ".$sender->getLevel()->getName());
+                            $sender->sendMessage("Yaw: ".$sender->getYaw());
+                            $sender->sendMessage("Pitch: ".$sender->getPitch());
+                        }
+                        else{
+                            $sender->sendMessage("§cYou don't have permissions to use this command.");
+                        }
+                    }
+                    else{
+                        $sender->sendMessage("§cPlease run this command in-game.");
+                    }
             }
             if(strtolower($args[0]) === "help"){
                 
@@ -88,12 +87,7 @@ class Loader extends PluginBase implements Listener{
     }
     
     public function onPlayerJoin(PlayerJoinEvent $event){
-        if(file_exists($this->getDataFolder()."data/".$event->getPlayer()->getName().".yml")){
-            
-        }
-        else{
-            $this->getServer()->getLogger()->notice("Created new data file for ".$event->getPlayer()->getName()." at LocatorPro\\data\\".$event->getPlayer()->getName().".yml");
-        }
+
     }
     
     public function onPlayerMove(PlayerMoveEvent $event){
