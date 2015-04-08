fc513f903a08754bc4252156478a4b6ba691d03c
Gamecrafter <pro_victor@ymail.com>
2015-04-07 18:50:34 -0700

    Update Loader.php

--- MyChat/src/mychat/Loader.php
+++ MyChat/src/mychat/Loader.php
@@ -16,16 +16,16 @@ class Loader extends PluginBase implements Listener{
     	$this->saveResource("key.txt");
     	if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
     	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-            $this->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
+            $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     	}
     	else{
-    	    $this->getLogger()->info("§eYour configuration file is outdated.");
+    	    $this->getServer()->getLogger()->warning("Your configuration file for ".$this->getDescription()->getFullName()." is outdated.");
     	    $this->getPluginLoader()->disablePlugin($this);
     	}
     }
     
     public function onDisable(){
-        $this->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+        $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onPlayerChat(PlayerChatEvent $event){
