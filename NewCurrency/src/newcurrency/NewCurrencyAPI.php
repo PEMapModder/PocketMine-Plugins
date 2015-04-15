2efccb6a9b9cd475823a87f7d488906f42bbb8c2
Gamecrafter <pro_victor@ymail.com>
2015-04-14 19:28:04 -0700

    Update NewCurrencyAPI.php

--- NewCurrency/src/newcurrency/NewCurrencyAPI.php
+++ NewCurrency/src/newcurrency/NewCurrencyAPI.php
@@ -4,7 +4,19 @@ namespace newcurrency;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\Player;
 
 class NewCurrencyAPI extends PluginBase{
 
+    public function onEnable(){
+      
+    }
+    
+    public function onDisable(){
+      
+    }
+    
+    public function getPlayerBalance(Player $player){
+      
+    }
 }
