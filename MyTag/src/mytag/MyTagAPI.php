10ac5be78f3b82f40725f5a848e3e83c51458bac
Gamecrafter <pro_victor@ymail.com>
2015-04-15 12:30:23 -0700

    Update MyTagAPI.php

--- MyTag/src/mytag/MyTagAPI.php
+++ MyTag/src/mytag/MyTagAPI.php
@@ -34,34 +34,4 @@ class MyTagAPI extends PluginBase implements Listener{
     	$this->tag->save();
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
-    public function onPlayerChat(PlayerChatEvent $event){
-    	//To-do
-    }
-    
-    public function onPlayerJoin(PlayerJoinEvent $event){
-    	if($this->getConfig()->get("enable")["auto-set"] === true){
-    	    if($this->tag->exists(strtolower($event->getPlayer()->getName()))){
-    	    	$event->getPlayer()->setNameTag($this->tag->get(strtolower($event->getPlayer()->getName())));
-    	    }
-    	    else{
-    	    	$this->tag->set(strtolower($event->getPlayer()->getName()), $event->getPlayer()->getNameTag());
-    	    	$this->tag->save();
-    	    	$this->getServer()->getLogger()->notice("Registered ".$event->getPlayer()->getName()." to MyTag at MyTag\\tag.yml");
-    	    }
-    	}
-    }
-	
-    public function onPlayerQuit(PlayerQuitEvent $event){
-    	if($this->getConfig()->get("enable")["auto-set"] === true){
-    	    if($this->tag->exists(strtolower($event->getPlayer()->getName()))){
-    	    	$this->tag->set(strtolower($event->getPlayer()->getName()), $event->getPlayer()->getNameTag());
-    	    }
-    	    else{
-    	    	$this->tag->set(strtolower($event->getPlayer()->getName()), $event->getPlayer()->getNameTag());
-    	    	$this->tag->save();
-    	    	$this->getServer()->getLogger()->notice("Registered ".$event->getPlayer()->getName()." to MyTag at MyTag\\tag.yml");
-    	    }
-    	}
-    }
 }
