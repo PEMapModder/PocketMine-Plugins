71be1b005edb4e3c003499dc8f5c096af1c40e94
Gamecrafter <pro_victor@ymail.com>
2015-04-15 12:14:17 -0700

    Update iManagerAPI.php

--- iManager/src/imanager/iManagerAPI.php
+++ iManager/src/imanager/iManagerAPI.php
@@ -42,28 +42,4 @@ class iManagerAPI extends PluginBase implements Listener{
         $this->ip->save();
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-
-    public function onPlayerChat(PlayerChatEvent $event){
-    	if($this->getConfig()->get("enable")["chat-log"] === true){
-    	    $this->chat->set($event->getPlayer()->getName().": ".$event->getMessage());
-    	}
-    }
-    
-    public function onPlayerCommandPreprocess(PlayerCommandPreprocessEvent $event){
-    	if($this->getConfig()->get("enable")["log-commands"] === true){
-    	    if($event->getMessage()[0] === "/"){
-    	    	$this->getServer()->getLogger()->notice($event->getPlayer()->getName()." issued command: ".$event->getMessage());
-    	    }
-    	}
-    }
-    
-    public function onPlayerPreLogin(PlayerPreLoginEvent $event){
-    	if($this->getConfig()->get("enable")["ip-whitelist"] === true){
-    	    if($this->ip->exists(strtolower($event->getPlayer()->getAddress()))){
-    	    }
-    	    else{
-    	    	$event->setCancelled();
-    	    }
-    	}
-    }
 }
