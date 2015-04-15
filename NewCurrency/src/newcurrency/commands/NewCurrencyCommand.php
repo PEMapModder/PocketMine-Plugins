a98703091209ef2325a02733dbc07a044d29bdf9
Gamecrafter <pro_victor@ymail.com>
2015-04-14 19:50:28 -0700

    Update NewCurrencyCommand.php

--- NewCurrency/src/newcurrency/commands/NewCurrencyCommand.php
+++ NewCurrency/src/newcurrency/commands/NewCurrencyCommand.php
@@ -2,6 +2,15 @@
 
 namespace newcurrency\commands;
 
-class NewCurrencyCommand{
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
 
+class NewCurrencyCommand implements CommandExecutor{
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "newcurrency"){
+          
+        }
+    }
 }
