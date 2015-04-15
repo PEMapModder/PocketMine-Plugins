a450f938e3b1839cd28e9e5c5c87cdbce19858be
Gamecrafter <pro_victor@ymail.com>
2015-04-14 21:29:04 -0700

    Update NewCurrencyListener.php

--- NewCurrency/src/newcurrency/NewCurrencyListener.php
+++ NewCurrency/src/newcurrency/NewCurrencyListener.php
@@ -7,8 +7,16 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 
 class NewCurrencyListener implements Listener{
-
+    
+	public function __construct(NewCurrencyAPI $plugin){
+		$this->plugin = $plugin;
+	}
     public function onPlayerJoin(PlayerJoinEvent $event){
-      
+        if($this->plugin->isRegistered(strtolower($event->getPlayer()->getName()))){
+            
+        }
+        else{
+            
+        }
     }
 }
