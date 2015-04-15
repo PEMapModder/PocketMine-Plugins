4e4eeea09bfafeb679c951dcf4236fb04bc68deb
Gamecrafter <pro_victor@ymail.com>
2015-04-14 20:49:15 -0700

    Update NewCurrencyCommand.php

--- NewCurrency/src/newcurrency/commands/NewCurrencyCommand.php
+++ NewCurrency/src/newcurrency/commands/NewCurrencyCommand.php
@@ -13,22 +13,22 @@ class NewCurrencyCommand implements CommandExecutor{
         if(strtolower($command->getName()) === "newcurrency"){
             if(isset($args[0])){
                 if(strtolower($args[0]) === "give"){
-                    
+                    return true;
                 }
                 if(strtolower($args[0]) === "help"){
-                    
+                    return true;
                 }
                 if(strtolower($args[0]) === "info"){
-                    
+                    return true;
                 }
                 if(strtolower($args[0]) === "send"){
-                    
+                    return true;
                 }
                 if(strtolower($args[0]) === "take"){
-                    
+                    return true;
                 }
                 if(strtolower($args[0]) === "view"){
-                    
+                    return true; 
                 }
                 return false;
             }
