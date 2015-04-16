9646bf1d5e5ab60b7c695a3a78a3b20e76205dd2
Gamecrafter <pro_victor@ymail.com>
2015-04-16 12:25:23 -0700

    Update ServerPopupsAPI.php

--- ServerPopups/src/serverpopups/ServerPopupsAPI.php
+++ ServerPopups/src/serverpopups/ServerPopupsAPI.php
@@ -24,11 +24,8 @@ class ServerPopupsAPI extends PluginBase{
         if(!file_exists($this->getDataFolder()."settings.yml")){
             $this->settings = new Config($this->getDataFolder()."settings.yml", Config::YAML);
             $this->settings->set("version", $this->getDescription()->getVersion());
+            $this->settings->save();
         }
-    }
-    
-    public function saveFiles(){
-        $this->settings->save();
     }
     
     public function updateFiles(){
