dc3060e8da55fd984d6d1ab50a81ed8669c61963
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:29:08 -0700

    Update Loader.php

--- DoMath/src/domath/Loader.php
+++ DoMath/src/domath/Loader.php
@@ -15,35 +15,4 @@ class Loader extends PluginBase{
     public function onDisable(){
         $this->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        if(strtolower($command->getName()) === "domath"){
-            if(isset($args[0])){
-                if(strtolower($args[0]) === "add"){
-                    return true;
-                }
-                if(strtolower($args[0]) === "divide"){
-                    return true;
-                }
-                if(strtolower($args[0]) === "help"){
-                    return true;
-                }
-                if(strtolower($args[0]) === "multiply"){
-                    return true;
-                }
-                if(strtolower($args[0]) === "subtract"){
-                    return true;
-                }
-                return false;
-            }
-            else{
-                $sender->sendMessage("DoMath commands");
-                $sender->sendMessage("/domath add -");
-                $sender->sendMessage("/domath divide -");
-                $sender->sendMessage("/domath help -");
-                $sender->sendMessage("/domath multiply -");
-                $sender->sendMessage("/domath subtract -");
-            }
-        }
-    }
 }
