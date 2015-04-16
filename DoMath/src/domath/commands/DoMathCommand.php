cae9b713e82bf7ec797055bc956a99dc5ef68930
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:30:21 -0700

    Create DoMathCommand.php

---
+++ DoMath/src/domath/commands/DoMathCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace domath\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+
+class DoMathCommand implements CommandExecutor{
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) === "domath"){
+            if(isset($args[0])){
+                if(strtolower($args[0]) === "add"){
+                    return true;
+                }
+                if(strtolower($args[0]) === "divide"){
+                    return true;
+                }
+                if(strtolower($args[0]) === "help"){
+                    return true;
+                }
+                if(strtolower($args[0]) === "multiply"){
+                    return true;
+                }
+                if(strtolower($args[0]) === "subtract"){
+                    return true;
+                }
+                return false;
+            }
+            else{
+                $sender->sendMessage("DoMath commands");
+                $sender->sendMessage("/domath add -");
+                $sender->sendMessage("/domath divide -");
+                $sender->sendMessage("/domath help -");
+                $sender->sendMessage("/domath multiply -");
+                $sender->sendMessage("/domath subtract -");
+            }
+        }
+    }
+}
