3e6d6da244cbfc2926503cd422f402614de7c4bf
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:11:37 -0700

    Create ServerPopupsCommand.php

---
+++ ServerPopups/src/serverpopups/commands/ServerPopupsCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace serverpopups\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use serverpopups\ServerPopupsAPI;
+
+class ServerPopupsCommand implements CommandExecutor{
+
+}
