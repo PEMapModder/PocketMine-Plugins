fb95caeafff05402117e917561fe9cedcab0fe10
Gamecrafter <pro_victor@ymail.com>
2015-04-15 21:29:37 -0700

    Create LocatorProCommand.php

---
+++ LocatorPro/src/locatorpro/commands/LocatorProCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace locatorpro;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class LocatorProCommand implements CommandExecutor{
+
+}
