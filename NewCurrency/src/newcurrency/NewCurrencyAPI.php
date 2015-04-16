36eff86844b888992033d9c677bbe63bf3a6fb0a
Gamecrafter <pro_victor@ymail.com>
2015-04-15 20:49:53 -0700

    Update NewCurrencyAPI.php

--- NewCurrency/src/newcurrency/NewCurrencyAPI.php
+++ NewCurrency/src/newcurrency/NewCurrencyAPI.php
@@ -28,9 +28,12 @@ class NewCurrencyAPI extends PluginBase{
     public function createFiles(){
     	if(!file_exists($this->getDataFolder()."account.yml")){
     	    $this->account = new Config($this->getDataFolder()."account.yml", Config::YAML);
+    	    $this->account->save();
     	}
     	if(!file_exists($this->getDataFolder()."settings.yml")){
     	    $this->settings = new Config($this->getDataFolder()."settings.yml", Config::YAML);
+    	    $this->settings->set("version", 1.0.0);
+    	    $this->settings->save();
     	}
     }
     
@@ -39,27 +42,27 @@ class NewCurrencyAPI extends PluginBase{
     }
     
     public function getCurrencyName(){
-        return $this->getConfig()->get("name");
+
     }
     
     public function getCurrencySymbol(){
-        return $this->getConfig()->get("symbol");
+
     }
     
     public function getMinimumBalance(){
-        return $this->getConfig()->get("balance");
+
     }
     
     public function getMaximumBalance(){
-        return $this->getConfig()->get("balance");
+
     }
     
     public function getBalance($player){
-        return $this->account->get($player);
+
     }
     
     public function getDefault(){
-        return $this->getConfig()->get("default");
+
     }
     
     public function decreaseBalance($player, $amount){
@@ -75,7 +78,7 @@ class NewCurrencyAPI extends PluginBase{
     }
     
     public function isRegistered($player){
-        return $this->account->exists($player);
+
     }
     
     public function register($player){
