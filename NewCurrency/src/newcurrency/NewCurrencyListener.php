2cf0909a1b7fbc0fc7fb7b43126a9669cf2b1077
Gamecrafter <pro_victor@ymail.com>
2015-04-15 20:56:44 -0700

    Update NewCurrencyListener.php

--- NewCurrency/src/newcurrency/NewCurrencyListener.php
+++ NewCurrency/src/newcurrency/NewCurrencyListener.php
@@ -12,19 +12,15 @@ class NewCurrencyListener implements Listener{
 	$this->plugin = $plugin;
 	$this->getPlugin()->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
     }
-    
-    public function getPlugin(){
-    	return $this->plugin;
-    }
-    
+
     public function onPlayerJoin(PlayerJoinEvent $event){
-        if($this->getPlugin()->isRegistered(strtolower($event->getPlayer()->getName()))){
+        if($this->plugin->isRegistered(strtolower($event->getPlayer()->getName()))){
             
         }
         else{
-            if($this->getPlugin()->getConfig()->get("enable")["auto-register"] === true){
-            	$this->getPlugin()->register(strtolower($event->getPlayer()->getName()));
-            	$this->getPlugin()->getServer()->getLogger()->notice("Registered ".$event->getPlayer()->getName()." to NewCurrency at NewCurrency\\account.yml");
+            if($this->plugin->settings->getNested("enable.auto-register") === true){
+            	$this->plugin->register(strtolower($event->getPlayer()->getName()));
+            	$this->plugin->getServer()->getLogger()->notice("Registered ".$event->getPlayer()->getName()." to NewCurrency at NewCurrency\\account.yml");
             }
         }
     }
