dbb95d8b97d1bf7a9dac29299a38e6c852466ff8
Gamecrafter <pro_victor@ymail.com>
2015-04-15 18:58:50 -0700

    Update ServerPopupsAPI.php

--- ServerPopups/src/serverpopups/ServerPopupsAPI.php
+++ ServerPopups/src/serverpopups/ServerPopupsAPI.php
@@ -6,4 +6,17 @@ use pocketmine\plugin\PluginBase;
 
 class ServerPopupsAPI extends PluginBase{
 
+    public function onEnable(){
+      
+    }
+    
+    public function onDisable(){
+      
+    }
+    
+    public function broadcastPopup($message){
+        foreach($this->getServer()->getOnlinePlayers() as $players){
+            $players->sendPopup($message);
+        }
+    }
 }
