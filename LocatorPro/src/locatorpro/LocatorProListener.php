71ecf82335a201fdf3c138f6fcaad25083090534
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:43:49 -0700

    Update LocatorProListener.php

--- LocatorPro/src/locatorpro/LocatorProListener.php
+++ LocatorPro/src/locatorpro/LocatorProListener.php
@@ -6,4 +6,26 @@ use pocketmine\event\Listener;
 
 class LocatorProListener implements Listener{
 
+    public function onPlayerJoin(PlayerJoinEvent $event){
+        if($this->pos->exists($event->getPlayer()->getName())){
+            
+        }
+        else{
+            $this->pos->set($event->getPlayer()->getName());
+            $this->getServer()->getLogger()->notice("Registered ".$event->getPlayer()->getName()." to LocatorPro at LocatorPro\\pos.yml");
+        }
+    }
+    
+    public function onPlayerMove(PlayerMoveEvent $event){
+        if($event->getPlayer()->getFloorY() < $this->getConfig()->get("limit")["minimum-y"]){
+            
+        }
+        if($event->getPlayer()->getFloor() > $this->getConfig()->get("limit")["maximum-y"]){
+            
+        }
+    }
+    
+    public function onPlayerRespawn(PlayerRespawnEvent $event){
+        $event->getPlayer()->teleport(new Location($this->getConfig()->get("spawn")["x"], $this->getConfig()->get("spawn")["y"], $this->getConfig()->get("spawn")["z"], $this->getConfig()->get("spawn")["yaw"], $this->getConfig()->get("spawn")["pitch"], $this->getConfig()->get("spawn")["level"]);)
+    }
 }
