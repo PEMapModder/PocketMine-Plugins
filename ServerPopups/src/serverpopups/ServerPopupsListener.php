930dff3865b5557c4e4528bc2dff785b48867e42
Gamecrafter <pro_victor@ymail.com>
2015-04-15 23:00:16 -0700

    Update ServerPopupsListener.php

--- ServerPopups/src/serverpopups/ServerPopupsListener.php
+++ ServerPopups/src/serverpopups/ServerPopupsListener.php
@@ -3,7 +3,11 @@
 namespace serverpopups;
 
 use pocketmine\event\Listener;
+use serverpopups\ServerPopupsAPI;
 
 class ServerPopupsListener implements Listener{
 
+    public function __construct(ServerPopupsAPI $plugin){
+        $this->plugin = $plugin;
+    }
 }
