32c3dbb745af6b75c5b5860b16584f580a47c985
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:31:24 -0700

    Update Loader.php

--- GlobalShield/src/globalshield/Loader.php
+++ GlobalShield/src/globalshield/Loader.php
@@ -2,20 +2,6 @@
 
 namespace globalshield;
 
-use pocketmine\block\BurningFurnace;
-use pocketmine\block\Chest;
-use pocketmine\block\Furnace;
-use pocketmine\block\IronDoor;
-use pocketmine\block\Stonecutter;
-use pocketmine\block\Trapdoor;
-use pocketmine\block\WoodDoor;
-use pocketmine\block\Workbench;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\player\PlayerBucketEmptyEvent;
-use pocketmine\event\player\PlayerBucketFillEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase implements Listener{
@@ -34,25 +20,5 @@ class Loader extends PluginBase implements Listener{
     
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
-    }
-    
-    public function onBlockBreak(BlockBreakEvent $event){
-
-    }
-    
-    public function onBlockPlace(BlockPlaceEvent $event){
-
-    }
-    
-    public function onPlayerBucketEmpty(PlayerBucketEmptyEvent $event){
-
-    }
-    
-    public function onPlayerBucketFill(PlayerBucketFillEvent $event){
-
-    }
-    
-    public function onPlayerInteract(PlayerInteractEvent $event){
-
     }
 }
