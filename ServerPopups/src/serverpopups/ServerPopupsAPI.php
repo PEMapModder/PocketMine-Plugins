125f86dd9406f7613b7652caa75653f6d5fc2f37
Gamecrafter <pro_victor@ymail.com>
2015-04-15 19:13:48 -0700

    Update ServerPopupsAPI.php

--- ServerPopups/src/serverpopups/ServerPopupsAPI.php
+++ ServerPopups/src/serverpopups/ServerPopupsAPI.php
@@ -14,6 +14,14 @@ class ServerPopupsAPI extends PluginBase{
       
     }
     
+    public function saveFiles(){
+        
+    }
+    
+    public function updateFiles(){
+        
+    }
+    
     public function broadcastPopup($message){
         foreach($this->getServer()->getOnlinePlayers() as $players){
             $players->sendPopup($message);
