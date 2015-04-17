e2e169d6e1abfd875828adb6ba2a7d4583396703
Gamecrafter <pro_victor@ymail.com>
2015-04-16 19:19:31 -0700

    Update Loader.php

--- SpreadFreeze/src/spreadfreeze/Loader.php
+++ SpreadFreeze/src/spreadfreeze/Loader.php
@@ -32,6 +32,24 @@ class Loader extends PluginBase implements Listener{
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
     
+    public function createFiles(){
+        if(!is_dir($this->getDataFolder())){
+            mkdir($this->getDataFolder());
+        }
+        if(!file_exists($this->getDataFolder()."settings.yml")){
+            $this->settings = new Config($this->getDataFolder()."settings.yml", Config::YAML);
+            $this->settings->set("version", $this->getDescription()->getVersion());
+            $this->settings->save();
+        }
+    }
+    
+    public function updateFiles(){
+        if(!$this->settings->get("version") === $this->getDescription()->getVersion()){
+            unlink($this->getDataFolder()."settings.yml");
+            $this->createFiles();
+        }
+    }
+    
     public function onBlockGrow(BlockGrowEvent $event){
         
     }
