85353e8df2461b6b09885ff7dc72a78e9c0b2ffb
Gamecrafter <pro_victor@ymail.com>
2015-04-16 20:50:05 -0700

    Update GlobalShieldLoader.php

--- GlobalShield/src/globalshield/GlobalShieldLoader.php
+++ GlobalShield/src/globalshield/GlobalShieldLoader.php
@@ -7,15 +7,8 @@ use pocketmine\plugin\PluginBase;
 class GlobalShieldLoader extends PluginBase implements Listener{
     
     public function onEnable(){
-    	$this->saveDefaultConfig();
-    	if($this->getConfig()->get("version") === $this->getDescription()->getVersion()){
-    	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-            $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
-    	}
-    	else{
-    	    $this->getServer()->getLogger()->warning("§eYour configuration file for ".$this->getDescription()->getFullName()." is outdated.");
-    	    $this->getPluginLoader()->disablePlugin($this);
-    	}
+    	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onDisable(){
