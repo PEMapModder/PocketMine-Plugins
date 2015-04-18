9358139dc29a039d77cff1668c552130e6ec9892
Gamecrafter <pro_victor@ymail.com>
2015-04-18 16:21:08 -0700

    Update TimeEditAPI.php

--- TimeEdit/src/timeedit/TimeEditAPI.php
+++ TimeEdit/src/timeedit/TimeEditAPI.php
@@ -3,11 +3,13 @@
 namespace timeedit;
 
 use pocketmine\plugin\PluginBase;
+use timeedit\TimeEditListener;
 
 class TimeEditAPI extends PluginBase{
 
     public function onEnable(){
-      
+        $this->listener = new TimeEditListener($this);
+        $this->getCommand("timeedit")->setExecutor(new commands\)
     }
     
     public function onDisable(){
