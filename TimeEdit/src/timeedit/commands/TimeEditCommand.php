6e03a2cacf593e50afa46e416df5d7adfd48d189
Gamecrafter <pro_victor@ymail.com>
2015-04-18 16:19:10 -0700

    Update TimeEditCommand.php

--- TimeEdit/src/timeedit/commands/TimeEditCommand.php
+++ TimeEdit/src/timeedit/commands/TimeEditCommand.php
@@ -12,11 +12,26 @@ class TimeEditCommand implements CommandExecutor{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "timeedit"){
             if(isset($args[0])){
+                if(strtolower($args[0]) === "help"){
+                    $sender->sendMessage("TimeEdit commands");
+                    return true;
+                }
+                if(strtolower($args[0]) === "info"){
+                    
+                }
                 if(strtolower($args[0]) === "lock"){
                     
                 }
+                if(strtolower($args[0]) === "set"){
+                    
+                }
+                if(strtolower($args[0]) === "unlock"){
+                    
+                }
+                return false;
             }
             else{
+                $sender->sendMessage("TimeEdit commands");
                 return true;
             }
         }
