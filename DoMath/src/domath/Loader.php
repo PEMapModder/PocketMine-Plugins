fcc53fe28e7d3341c1cea95288c691ae10097b31
Gamecrafter <pro_victor@ymail.com>
2015-04-20 20:35:11 -0700

    Update Loader.php

--- DoMath/src/domath/Loader.php
+++ DoMath/src/domath/Loader.php
@@ -2,17 +2,15 @@
 
 namespace domath;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
     
     public function onEnable(){
-        $this->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
+        $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onDisable(){
-        $this->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+        $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
 }
