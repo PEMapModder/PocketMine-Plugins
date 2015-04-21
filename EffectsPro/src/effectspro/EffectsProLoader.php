a298d1f8fd223b6d6a92f4a59987e5dc72008b1d
Gamecrafter <pro_victor@ymail.com>
2015-04-20 20:34:41 -0700

    Update EffectsProLoader.php

--- EffectsPro/src/effectspro/EffectsProLoader.php
+++ EffectsPro/src/effectspro/EffectsProLoader.php
@@ -7,10 +7,10 @@ use pocketmine\plugin\PluginBase;
 class EffectsProLoader extends PluginBase{
 
     public function onEnable(){
-        $this->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
+        $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onDisable(){
-        $this->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+        $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
 }
