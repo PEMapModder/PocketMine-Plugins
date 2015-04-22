940a9cbf025089297349b45d402d819ed8bac324
Gamecrafter <pro_victor@ymail.com>
2015-04-21 21:00:32 -0700

    Update LocatorProCommand.php

--- LocatorPro/src/locatorpro/commands/LocatorProCommand.php
+++ LocatorPro/src/locatorpro/commands/LocatorProCommand.php
@@ -12,6 +12,7 @@ class LocatorProCommand implements CommandExecutor{
 
     public function __construct(LocatorProAPI $plugin){
         $this->plugin = $plugin;
+        $this->plugin->getCommand("locatorpro")->setExecutor($this);
     }
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
