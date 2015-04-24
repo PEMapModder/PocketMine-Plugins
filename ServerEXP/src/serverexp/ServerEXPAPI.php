ea3d1e844931c0284e4fd4b9ca40261611e3fcbd
Gamecrafter <pro_victor@ymail.com>
2015-04-24 12:39:03 -0700

    Update ServerEXPAPI.php

--- ServerEXP/src/serverexp/ServerEXPAPI.php
+++ ServerEXP/src/serverexp/ServerEXPAPI.php
@@ -6,4 +6,27 @@ use pocketmine\plugin\PluginBase;
 
 class ServerEXPAPI extends PluginBase{
 
+    public function onEnable(){
+      
+    }
+    
+    public function onDisable(){
+      
+    }
+    
+    public function getEXP($player){
+      
+    }
+    
+    public function decreaseEXP($player){
+      
+    }
+    
+    public function increaseEXP($player){
+      
+    }
+    
+    public function setEXP($player){
+      
+    }
 }
