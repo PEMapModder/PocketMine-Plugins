6fce8426b32c5310fd3ad2af750118eb54b46a0b
Gamecrafter <pro_victor@ymail.com>
2015-04-30 19:20:07 -0700

    Update AutoEffectTask.php

--- EffectsPro/src/effectspro/tasks/AutoEffectTask.php
+++ EffectsPro/src/effectspro/tasks/AutoEffectTask.php
@@ -6,4 +6,8 @@ use pocketmine\scheduler\PluginTask;
 
 class AutoEffectTask extends PluginTask{
   
+    public function __construct(EffectsProAPI $plugin){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
 }
