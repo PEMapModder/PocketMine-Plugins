cd427be294322caa530d585b4c3d18c5936c32ab
Gamecrafter <pro_victor@ymail.com>
2015-05-11 19:25:11 -0700

    Update FistBlasterLoader.php

--- FistBlaster/src/fistblaster/FistBlasterLoader.php
+++ FistBlaster/src/fistblaster/FistBlasterLoader.php
@@ -10,50 +10,50 @@ use pocketmine\event\Listener;
 use pocketmine\level\Explosion;
 use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 
 class FistBlasterLoader extends PluginBase implements Listener{
 
     public function onEnable(){
-        $this->blast = array();
+        $this->fistblaster = array();
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
    
-	public function onDisable(){
-		$this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
-	}
+    public function onDisable(){
+    	unset($this->fistblaster);
+	$this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+    }
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "fistblaster"){
             if($sender instanceof Player){
-                if(isset($this->blast[$sender->getName()])){
-                    unset($this->blast[$sender->getName()]);
-                    $sender->sendMessage(TextFormat::GREEN."You will now no longer blow up blocks you touch.");
+                if(isset($this->fistblaster[$sender->getName()])){
+                    unset($this->fistblaster[$sender->getName()]);
+                    $sender->sendMessage("§aYou will now no longer blow up blocks you touch.");
                 }
                 else{
-                    $this->blast[$sender->getName()] = true;
-                    $sender->sendMessage(TextFormat::GREEN."You will now blow up all blocks you touch.");
+                    $this->fistblaster[$sender->getName()] = true;
+                    $sender->sendMessage("§aYou will now blow up all blocks you touch.");
                 }
             }
             else{
-                $sender->sendMessage(TextFormat::RED."You can only use FistBlaster in-game.");
+                $sender->sendMessage("§cPlease run this command in-game.");
             }
             return true;
         }
     }
     
     public function onPlayerInteract(PlayerInteractEvent $event){
-        if(isset($this->blast[$event->getPlayer()->getName()])){
-            $this->explode = new Explosion(new Position($event->getBlock()->getX(), $event->getBlock()->getY(), $event->getBlock()->getZ(), $event->getBlock()->getLevel()), $this->getConfig()->get("size"));
-		$this->explode->explode();
+        if(isset($this->fistblaster[$event->getPlayer()->getName()])){
+            $explosion = new Explosion(new Position($event->getBlock()->getX(), $event->getBlock()->getY(), $event->getBlock()->getZ(), $event->getBlock()->getLevel()), $this->getConfig()->get("size"));
+	    $explosion->explode();
         }
     }
     
     public function onPlayerQuit(PlayerQuitEvent $event){
-        if(isset($this->blast[$event->getPlayer()->getName()])){
-            unset($this->blast[$event->getPlayer()->getName()]);
+        if(isset($this->fistblaster[$event->getPlayer()->getName()])){
+            unset($this->fistblaster[$event->getPlayer()->getName()]);
         }
     }
 }
