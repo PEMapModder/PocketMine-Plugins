23784d6930f03ffe231de35948974586b702a26f
Gamecrafter <pro_victor@ymail.com>
2015-05-12 12:25:27 -0700

    Update ServerHelpLoader.php

--- ServerHelp/src/serverhelp/ServerHelpLoader.php
+++ ServerHelp/src/serverhelp/ServerHelpLoader.php
@@ -2,8 +2,26 @@
 
 namespace serverhelp;
 
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 
-class ServerHelpLoader extends PluginBase{
+class ServerHelpLoader extends PluginBase implements Listener{
 
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onDisable(){
+      	$this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function onPlayerCommandPreprocess(PlayerCommandPreprocess $event){
+        $command = explode(" ", strtolower($event->getMessage()));
+        if($command[0] === "/help"){
+          
+        }
+    }
 }
