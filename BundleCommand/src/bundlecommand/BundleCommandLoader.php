548c17b7eec02f33d985c4f19f85403dfae32285
Gamecrafter <pro_victor@ymail.com>
2015-05-12 12:36:22 -0700

    Update BundleCommandLoader.php

--- BundleCommand/src/bundlecommand/BundleCommandLoader.php
+++ BundleCommand/src/bundlecommand/BundleCommandLoader.php
@@ -2,6 +2,8 @@
 
 namespace bundlecommand;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class BundleCommandLoader extends PluginBase{
