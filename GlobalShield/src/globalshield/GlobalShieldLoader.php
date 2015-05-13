c67084309d9c795d7c0f0b0ceca35be576c3bdb5
Gamecrafter <pro_victor@ymail.com>
2015-05-13 12:26:09 -0700

    Update GlobalShieldLoader.php

--- GlobalShield/src/globalshield/GlobalShieldLoader.php
+++ GlobalShield/src/globalshield/GlobalShieldLoader.php
@@ -8,11 +8,16 @@ use pocketmine\plugin\PluginBase;
 class GlobalShieldLoader extends PluginBase{
     
     public function onEnable(){
+        $this->saveFiles();
         $this->listener = new GlobalShieldListener($this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function saveFiles(){
+        
     }
 }
