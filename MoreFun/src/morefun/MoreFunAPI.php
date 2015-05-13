22dcbc8063c17403d4183598d13bc8b8f2e1963a
Gamecrafter <pro_victor@ymail.com>
2015-05-13 13:15:38 -0700

    Update MoreFunAPI.php

--- MoreFun/src/morefun/MoreFunAPI.php
+++ MoreFun/src/morefun/MoreFunAPI.php
@@ -3,6 +3,7 @@
 namespace morefun;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
 
 class MoreFunAPI extends PluginBase{
   
@@ -11,6 +12,14 @@ class MoreFunAPI extends PluginBase{
     }
     
     public function onDisable(){
+      
+    }
+    
+    public function setPlayerFreeze(Player $player, $value){
+      
+    }
+    
+    public function setPlayerInventoryLock(Player $player, $value){
       
     }
 }
