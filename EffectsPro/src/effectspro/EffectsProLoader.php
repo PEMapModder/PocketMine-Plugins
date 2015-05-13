94389afec56b05620ef43eb1830573c24ad34417
Gamecrafter <pro_victor@ymail.com>
2015-05-13 12:37:36 -0700

    Update EffectsProLoader.php

--- EffectsPro/src/effectspro/EffectsProLoader.php
+++ EffectsPro/src/effectspro/EffectsProLoader.php
@@ -7,10 +7,17 @@ use pocketmine\plugin\PluginBase;
 class EffectsProLoader extends PluginBase{
 
     public function onEnable(){
+        $this->saveFiles();
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
     
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
+    }
+    
+    public function saveFiles(){
+        if(!file_exists($this->getDataFolder()."config.yml")){
+            $this->saveDefaultConfig();
+        }
     }
 }
