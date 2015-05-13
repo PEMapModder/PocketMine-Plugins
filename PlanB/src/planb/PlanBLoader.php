7b15d202a234feb520c262903695cbf6b64d3d89
Gamecrafter <pro_victor@ymail.com>
2015-05-12 21:39:55 -0700

    Update PlanBLoader.php

--- PlanB/src/planb/PlanBLoader.php
+++ PlanB/src/planb/PlanBLoader.php
@@ -25,52 +25,36 @@ class PlanBLoader extends PluginBase{
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "planb"){
-            if(isset($args[0])){
-                if(strtolower($args[0]) === "help"){
-                    $sender->sendMessage("§bPlanB commands");
-                    $sender->sendMessage("§a/planb help §c- §fShows all sub-commands for /planb");
-                    $sender->sendMessage("§a/planb restore §c- §fRestores OP status of all online players listed in backup.txt");
-                    return true;
-                }
-                if(strtolower($args[0]) === "restore"){
-                    if($sender instanceof Player){
-                        if($this->backup->exists(strtolower($sender->getName()))){
-                            $sender->sendMessage("§eRestoring the OP status of all OPs...");
-                            foreach($this->getServer()->getOnlinePlayers() as $player){
-                                if($this->backup->exists(strtolower($player->getName()))){
-                                    if($player->isOp()){
-                                    }
-                                    else{
-                                        $player->setOp(true);
-                                        $player->sendMessage("§aYour OP status has been restored.");
-                                        $sender->sendMessage("§aRestored ".$player->getName()."'s OP status.");
-                                    }
-                                }
-                                else{
-                                    if($player->isOp()){
-                                        $player->setOp(false);
-                                        $player->kick("Detected potential hacker");
-                                        $this->getServer()->broadcastMessage("§eDeopped and kicked potential hacker: ".$player->getName());
-                                    }
-                                }
+            if($sender instanceof Player){
+                if($this->backup->exists(strtolower($sender->getName()))){
+                    $sender->sendMessage("§eRestoring the OP status of all OPs...");
+                    foreach($this->getServer()->getOnlinePlayers() as $player){
+                        if($this->backup->exists(strtolower($player->getName()))){
+                            if($player->isOp()){
+                            }
+                            else{
+                                $player->setOp(true);
+                                $player->sendMessage("§aYour OP status has been restored.");
+                                $sender->sendMessage("§aRestored ".$player->getName()."'s OP status.");
                             }
                         }
                         else{
-                            $sender->sendMessage("§cYou don't have permissions to restore OP statuses.");
+                            if($player->isOp()){
+                                $player->setOp(false);
+                                $player->kick("Detected potential hacker");
+                                $this->getServer()->broadcastMessage("§eDeopped and kicked potential hacker: ".$player->getName());
+                            }
                         }
                     }
-                    else{
-                        $sender->sendMessage("§cPlease run this command in-game.");
-                    }
-                    return true;
+                }
+                else{
+                    $sender->sendMessage("§cYou don't have permissions to restore OP statuses.");
                 }
             }
             else{
-                $sender->sendMessage("§bPlanB commands");
-                $sender->sendMessage("§a/planb help §c- §fShows all sub-commands for /planb");
-                $sender->sendMessage("§a/planb restore §c- §fRestores OP status of all online players listed in backup.txt");
-                return true;
+                $sender->sendMessage("§cPlease run this command in-game.");
             }
+            return true;
         }
     }
 }
