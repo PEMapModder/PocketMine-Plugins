7096318202cd518bfbe287eacd0394e477066360
Gamecrafter <pro_victor@ymail.com>
2015-05-18 21:33:12 -0700

    Update ServerEXPAPI.php

--- ServerEXP/src/serverexp/ServerEXPAPI.php
+++ ServerEXP/src/serverexp/ServerEXPAPI.php
@@ -21,9 +21,6 @@ class ServerEXPAPI extends PluginBase{
             mkdir($this->getDataFolder());
         }
         if(file_exists($this->getDataFolder()."config.yml")){
-            if(!is_bool($this->getConfig()->getNested("enable.auto-register"))){
-                $this->getConfig()->setNested("enable.auto-register", true);
-            }
             if(!is_numeric($this->getConfig()->get("default"))){
                 $this->getConfig()->set("default", 0);
             }
