27596c946bd5614e48aca017641e9452a004bcc5
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:41:05 -0700

    Update TimeEssentialsAPI.php

--- TimeEssentials/src/timeessentials/TimeEssentialsAPI.php
+++ TimeEssentials/src/timeessentials/TimeEssentialsAPI.php
@@ -14,11 +14,9 @@ class TimeEssentialsAPI extends PluginBase{
         $this->listener = new TimeEssentialsListener($this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function saveFiles(){
         if(file_exists($this->getDataFolder()."config.yml")){
             
