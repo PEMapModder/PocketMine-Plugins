2d1d4edd226a93695ecf9f65f0f8a3d355b4dd98
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:33:53 -0700

    Update and rename EffectsProLoader.php to EffectsProAPI.php

--- EffectsPro/src/effectspro/EffectsProAPI.php
+++ EffectsPro/src/effectspro/EffectsProAPI.php
@@ -5,17 +5,15 @@ namespace effectspro;
 use effectspro\task\AutoEffectTask;
 use pocketmine\plugin\PluginBase;
 
-class EffectsProLoader extends PluginBase{
+class EffectsProAPI extends PluginBase{
 
     public function onEnable(){
         $this->saveFiles();
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function saveFiles(){
         if(!file_exists($this->getDataFolder()."config.yml")){
             $this->saveDefaultConfig();
