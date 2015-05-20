42fc91ae2a976eac89705256ed863fd5fabfc326
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:35:01 -0700

    Update GlobalShieldAPI.php

--- GlobalShield/src/globalshield/GlobalShieldAPI.php
+++ GlobalShield/src/globalshield/GlobalShieldAPI.php
@@ -14,21 +14,17 @@ class GlobalShieldAPI extends PluginBase{
         $this->listener = new GlobalShieldListener($this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function saveFiles(){
         if(!file_exists($this->getDataFolder()."config.yml")){
             $this->saveDefaultConfig();
         }
     }
-    
     public function isItemBannedInLevel(Item $item, Level $level){
         return in_array($item->getId(), $this->getConfig()->getNested("level.".strtolower($level->getName())."items"));
     }
-    
     public function isLevelProtected(Level $level){
         
     }
