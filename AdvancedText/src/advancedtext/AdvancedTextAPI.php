f01caa1e39c8a925464adf9fc7d6ecf02ce5dcce
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:32:14 -0700

    Update AdvancedTextAPI.php

--- AdvancedText/src/advancedtext/AdvancedTextAPI.php
+++ AdvancedText/src/advancedtext/AdvancedTextAPI.php
@@ -18,29 +18,24 @@ class AdvancedTextAPI extends PluginBase{
         $this->listener = new AdvancedTextListener($this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
 	$this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function saveFiles(){
         if(!file_exists($this->getDataFolder())){
             mkdir($this->getDataFolder());
         }
     }
-    
     public function broadcastPopup($message){
         foreach($this->getServer()->getOnlinePlayers() as $player){
             $player->sendPopup($message);
         }
     }
-    
     public function broadcastTip($message){
     	foreach($this->getServer()->getOnlinePlayers() as $player){
     	    $player->sendTip($message);
     	}
     }
-    
     public function createFloatingText(Player $player, Vector3 $vector, $text, $title){
     	$player->getLevel()->addParticle(new FloatingTextParticle($vector, $text, $title), $this->getServer()->getOnlinePlayers());
     }
