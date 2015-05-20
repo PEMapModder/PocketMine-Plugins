35b4db620ef3093e148a61aa5883de631ef1d236
Gamecrafter <pro_victor@ymail.com>
2015-05-19 21:37:06 -0700

    Update AutoMessageTask.php

--- AdvancedText/src/advancedtext/task/AutoMessageTask.php
+++ AdvancedText/src/advancedtext/task/AutoMessageTask.php
@@ -8,6 +8,7 @@ use pocketmine\scheduler\PluginTask;
 class AutoMessageTask extends PluginTask{
 
     public function __construct(AdvancedTextAPI $plugin){
+        parent::__construct($plugin);
         $this->plugin = $plugin;
         $this->getPlugin()->getServer()->getScheduler()->scheduleRepeatingTask($this, );
     }
