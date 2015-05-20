2250ab8d4732f63adf7ef7d7545fc4b6e1427da4
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:33:20 -0700

    Update DoMathAPI.php

--- DoMath/src/domath/DoMathAPI.php
+++ DoMath/src/domath/DoMathAPI.php
@@ -10,7 +10,6 @@ class DoMathAPI extends PluginBase{
     public function onEnable(){
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
         $this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
