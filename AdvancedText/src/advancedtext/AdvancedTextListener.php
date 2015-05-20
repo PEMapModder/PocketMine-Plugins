f682a34b17094f8e8ae823d6f58d913ae2d36d2a
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:32:29 -0700

    Update AdvancedTextListener.php

--- AdvancedText/src/advancedtext/AdvancedTextListener.php
+++ AdvancedText/src/advancedtext/AdvancedTextListener.php
@@ -10,13 +10,11 @@ class AdvancedTextListener implements Listener{
 
     public function __construct(ServerPopupsAPI $plugin){
         $this->plugin = $plugin;
-        $this->getPlugin()->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
+        $this->getPlugin()->getServer()->getPluginManager()->registerEvents($this, $this->getPlugin());
     }
-    
     public function getPlugin(){
         return $this->plugin;
     }
-    
     public function onSignChange(SignChangeEvent $event){
         
     }
