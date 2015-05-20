e0787a0b8b95a3f4ce7f8fa3d349a06fbee1eded
Gamecrafter <pro_victor@ymail.com>
2015-05-20 13:40:43 -0700

    Update ServerHelpLoader.php

--- ServerHelp/src/serverhelp/ServerHelpLoader.php
+++ ServerHelp/src/serverhelp/ServerHelpLoader.php
@@ -13,17 +13,14 @@ class ServerHelpLoader extends PluginBase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getLogger()->info("§aEnabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function onDisable(){
       	$this->getServer()->getLogger()->info("§cDisabling ".$this->getDescription()->getFullName()."...");
     }
-    
     public function saveFiles(){
         if(!file_exists($this->getDataFolder()."config.yml")){
             $this->saveDefaultConfig();
         }
     }
-    
     public function onPlayerCommandPreprocess(PlayerCommandPreprocess $event){
         $command = explode(" ", strtolower($event->getMessage()));
         if($command[0] === "/help"){
